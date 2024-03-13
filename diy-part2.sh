02e1da80464894c772c0510a5f2633cd03d8b24d
asxs123 <54346276+asxs123@users.noreply.github.com>
2024-03-13 12:24:07 +0800

    1

--- diy-part2.sh
+++ diy-part2.sh
@@ -36,6 +36,12 @@ is_wsl2op=$1
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate
 
+# 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
+# sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
+
+###### 取消bootstrap为默认主题 ######
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
 rm -rf ./package/lean/luci-app-adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/lean/luci-app-adguardhome
 

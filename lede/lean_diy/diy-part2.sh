b08a746770c5e01fc27f9b54e0176e362c218792
asxs123 <54346276+asxs123@users.noreply.github.com>
2024-03-17 23:04:35 +0800

    1

--- lede/lean_diy/diy-part2.sh
+++ lede/lean_diy/diy-part2.sh
@@ -15,6 +15,12 @@ is_wsl2op=$1
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate
 
+# 修改主机名字，把 X86 修改你喜欢的就行（不能纯数字或者使用中文）
+# sed -i 's/OpenWrt/X86/g' package/base-files/files/bin/config_generate
+
+###### 取消bootstrap为默认主题 ######
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
 # Modify default passwd
 sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./ d' package/lean/default-settings/files/zzz-default-settings
 
@@ -27,6 +33,10 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git ./pack
 
 rm -rf ./package/lean/luci-app-adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/lean/luci-app-adguardhome
+
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git ./package/ddns-go
+
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git ./package/luci-app-unblockneteasemusic
 
 # mosdns
 
@@ -90,3 +100,32 @@ fi
 # Diy
 # rm -rf ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 # wget -P ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status https://github.com/smallprogram/OpenWrtAction/raw/main/source/openwrtfile/index.htm
+
+
+echo -e "预置Clash内核"
+mkdir -p feeds/OpenClash/luci-app-openclash/root/etc/openclash/core
+core_path="feeds/OpenClash/luci-app-openclash/root/etc/openclash/core"
+# goe_path="luci-app-openclash/root/etc/openclash"
+
+CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz"
+CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep $1 | awk -F '"' '{print $4}' | grep "v3" )
+CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz"
+# GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+# GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+
+wget -qO- $CLASH_DEV_URL | tar xOvz > $core_path/clash
+wget -qO- $CLASH_TUN_URL | gunzip -c > $core_path/clash_tun
+wget -qO- $CLASH_META_URL | tar xOvz > $core_path/clash_meta
+# wget -qO- $GEOIP_URL > $goe_path/GeoIP.dat
+# wget -qO- $GEOSITE_URL > $goe_path/GeoSite.dat
+
+chmod +x $core_path/clash*
+
+echo -e "预置unblockneteasemusic内核"
+NAME="package/luci-app-unblockneteasemusic/root/usr/share/unblockneteasemusic" && mkdir -p $NAME/core
+echo "$(uclient-fetch -qO- 'https://api.github.com/repos/UnblockNeteaseMusic/server/commits?sha=enhanced&path=precompiled' | jsonfilter -e '@[0].sha')">"$NAME/core_local_ver"
+curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/precompiled/app.js -o $NAME/core/app.js
+curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/precompiled/bridge.js -o $NAME/core/bridge.js
+curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/ca.crt -o $NAME/core/ca.crt
+curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/server.crt -o $NAME/core/server.crt
+curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/server.key -o $NAME/core/server.key

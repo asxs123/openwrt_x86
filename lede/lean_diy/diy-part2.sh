7ff8d4acb87a9c96e7c96a4338b986fc2147f19d
asxs123 <54346276+asxs123@users.noreply.github.com>
2024-03-19 16:46:24 +0800

    1

--- lede/lean_diy/diy-part2.sh
+++ lede/lean_diy/diy-part2.sh
@@ -13,34 +13,20 @@
 is_wsl2op=$1
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate
-
-# 修改主机名字，把 X86 修改你喜欢的就行（不能纯数字或者使用中文）
-# sed -i 's/OpenWrt/X86/g' package/base-files/files/bin/config_generate
-
-###### 取消bootstrap为默认主题 ######
-sed -i 's/luci-theme-bootstrap/luci-theme-argonnew/g' feeds/luci/collections/luci/Makefile
+sed -i 's/192.168.1.1/10.10.0.253/g' package/base-files/files/bin/config_generate
 
 # Modify default passwd
 sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./ d' package/lean/default-settings/files/zzz-default-settings
 
 # Add Theme
 rm -rf ./feeds/luci/themes/luci-theme-argon
-# rm -rf ./feeds/luci/themes/luci-theme-argon-mod
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./feeds/luci/themes/luci-theme-argon
-# git clone https://github.com/jjm2473/luci-theme-argon.git ./feeds/luci/themes/luci-theme-argonnew
 
 rm -rf ./package/lean/luci-app-argon-config
 git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-app-argon-config
-# git clone https://github.com/jjm2473/luci-app-argon-config.git ./package/lean/luci-app-argon-confignew
 
 rm -rf ./package/lean/luci-app-adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/lean/luci-app-adguardhome
-
-git clone https://github.com/sirpdboy/luci-app-ddns-go.git ./package/ddns-go
-
-rm -rf ./feeds/luci/applications/luci-app-unblockmusic
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git ./package/luci-app-unblockneteasemusic
 
 # mosdns
 
@@ -104,8 +90,6 @@ fi
 # Diy
 # rm -rf ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 # wget -P ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status https://github.com/smallprogram/OpenWrtAction/raw/main/source/openwrtfile/index.htm
-
-
 echo -e "预置Clash内核"
 mkdir -p feeds/OpenClash/luci-app-openclash/root/etc/openclash/core
 core_path="feeds/OpenClash/luci-app-openclash/root/etc/openclash/core"
@@ -125,6 +109,8 @@ wget -qO- $CLASH_META_URL | tar xOvz > $core_path/clash_meta
 
 chmod +x $core_path/clash*
 
+
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git ./package/luci-app-unblockneteasemusic
 echo -e "预置unblockneteasemusic内核"
 NAME="package/luci-app-unblockneteasemusic/root/usr/share/unblockneteasemusic" && mkdir -p $NAME/core
 echo "$(uclient-fetch -qO- 'https://api.github.com/repos/UnblockNeteaseMusic/server/commits?sha=enhanced&path=precompiled' | jsonfilter -e '@[0].sha')">"$NAME/core_local_ver"

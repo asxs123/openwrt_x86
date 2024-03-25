ce5d40f5ea77b3684b9b8367f3bc4aab0d984414
asxs123 <54346276+asxs123@users.noreply.github.com>
2024-03-25 17:27:21 +0800

    1

--- lede/lean_diy/diy-part2.sh
+++ lede/lean_diy/diy-part2.sh
@@ -20,10 +20,10 @@ sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./ d' package/lean/default-settings/fi
 
 # Add Theme
 rm -rf ./feeds/luci/themes/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./feeds/luci/themes/luci-theme-argon
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./feeds/luci/themes/luci-theme-argon
 
 rm -rf ./package/lean/luci-app-argon-config
-git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-app-argon-config
+# git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-app-argon-config
 
 rm -rf ./package/lean/luci-app-adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/lean/luci-app-adguardhome
@@ -56,13 +56,13 @@ git clone https://github.com/sbwml/luci-app-mosdns -b v5 ./package/custom_packag
 # sed -i 's/console=tty0//g'  target/linux/x86/image/Makefile
 
 
-rm -rf ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background
-mkdir -p ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background
+# rm -rf ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background
+# mkdir -p ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background
 
 if [ ! -n "$is_wsl2op" ]; then
     # Add default login background
-    cp -r $GITHUB_WORKSPACE/source/video/* ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background/
-    cp -r $GITHUB_WORKSPACE/source/img/* ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background/
+    # cp -r $GITHUB_WORKSPACE/source/video/* ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background/
+    # cp -r $GITHUB_WORKSPACE/source/img/* ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background/
 
     # Inject download package
     mkdir -p $GITHUB_WORKSPACE/openwrt/dl
@@ -73,8 +73,8 @@ if [ ! -n "$is_wsl2op" ]; then
 
 else
     # Add default login background
-    cp -r /home/$USER/OpenWrtAction/source/video/* ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background/
-    cp -r /home/$USER/OpenWrtAction/source/img/* ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background/
+    # cp -r /home/$USER/OpenWrtAction/source/video/* ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background/
+    # cp -r /home/$USER/OpenWrtAction/source/img/* ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background/
 
     # Inject download package
     mkdir -p dl
@@ -90,32 +90,33 @@ fi
 # Diy
 # rm -rf ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 # wget -P ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status https://github.com/smallprogram/OpenWrtAction/raw/main/source/openwrtfile/index.htm
-echo -e "预置Clash内核"
-mkdir -p feeds/OpenClash/luci-app-openclash/root/etc/openclash/core
-core_path="feeds/OpenClash/luci-app-openclash/root/etc/openclash/core"
-# goe_path="luci-app-openclash/root/etc/openclash"
 
-CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz"
-CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep $1 | awk -F '"' '{print $4}' | grep "v3" )
-CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz"
-# GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
-# GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+# echo -e "预置Clash内核"
+# mkdir -p feeds/OpenClash/luci-app-openclash/root/etc/openclash/core
+# core_path="feeds/OpenClash/luci-app-openclash/root/etc/openclash/core"
+# # goe_path="luci-app-openclash/root/etc/openclash"
 
-wget -qO- $CLASH_DEV_URL | tar xOvz > $core_path/clash
-wget -qO- $CLASH_TUN_URL | gunzip -c > $core_path/clash_tun
-wget -qO- $CLASH_META_URL | tar xOvz > $core_path/clash_meta
-# wget -qO- $GEOIP_URL > $goe_path/GeoIP.dat
-# wget -qO- $GEOSITE_URL > $goe_path/GeoSite.dat
+# CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz"
+# CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep $1 | awk -F '"' '{print $4}' | grep "v3" )
+# CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz"
+# # GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+# # GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
 
-chmod +x $core_path/clash*
+# wget -qO- $CLASH_DEV_URL | tar xOvz > $core_path/clash
+# wget -qO- $CLASH_TUN_URL | gunzip -c > $core_path/clash_tun
+# wget -qO- $CLASH_META_URL | tar xOvz > $core_path/clash_meta
+# # wget -qO- $GEOIP_URL > $goe_path/GeoIP.dat
+# # wget -qO- $GEOSITE_URL > $goe_path/GeoSite.dat
+
+# chmod +x $core_path/clash*
 
 
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git ./package/luci-app-unblockneteasemusic
-echo -e "预置unblockneteasemusic内核"
-NAME="package/luci-app-unblockneteasemusic/root/usr/share/unblockneteasemusic" && mkdir -p $NAME/core
-echo "$(uclient-fetch -qO- 'https://api.github.com/repos/UnblockNeteaseMusic/server/commits?sha=enhanced&path=precompiled' | jsonfilter -e '@[0].sha')">"$NAME/core_local_ver"
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/precompiled/app.js -o $NAME/core/app.js
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/precompiled/bridge.js -o $NAME/core/bridge.js
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/ca.crt -o $NAME/core/ca.crt
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/server.crt -o $NAME/core/server.crt
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/server.key -o $NAME/core/server.key
+# git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git ./package/luci-app-unblockneteasemusic
+# echo -e "预置unblockneteasemusic内核"
+# NAME="package/luci-app-unblockneteasemusic/root/usr/share/unblockneteasemusic" && mkdir -p $NAME/core
+# echo "$(uclient-fetch -qO- 'https://api.github.com/repos/UnblockNeteaseMusic/server/commits?sha=enhanced&path=precompiled' | jsonfilter -e '@[0].sha')">"$NAME/core_local_ver"
+# curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/precompiled/app.js -o $NAME/core/app.js
+# curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/precompiled/bridge.js -o $NAME/core/bridge.js
+# curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/ca.crt -o $NAME/core/ca.crt
+# curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/server.crt -o $NAME/core/server.crt
+# curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/server.key -o $NAME/core/server.key

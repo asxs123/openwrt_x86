09c4f1f96bdb821addb63363dc32afe951e86471
asxs123 <54346276+asxs123@users.noreply.github.com>
2024-03-27 04:39:36 +0800

    1

--- lede/lean_diy/diy-part2.sh
+++ lede/lean_diy/diy-part2.sh
@@ -98,24 +98,24 @@ fi
 # rm -rf ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 # wget -P ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status https://github.com/smallprogram/OpenWrtAction/raw/main/source/openwrtfile/index.htm
 
-# echo -e "预置Clash内核"
-# mkdir -p feeds/OpenClash/luci-app-openclash/root/etc/openclash/core
-# core_path="feeds/OpenClash/luci-app-openclash/root/etc/openclash/core"
-# # goe_path="luci-app-openclash/root/etc/openclash"
+echo -e "预置Clash内核"
+mkdir -p feeds/OpenClash/luci-app-openclash/root/etc/openclash/core
+core_path="feeds/OpenClash/luci-app-openclash/root/etc/openclash/core"
+# goe_path="luci-app-openclash/root/etc/openclash"
 
-# CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz"
-# CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep $1 | awk -F '"' '{print $4}' | grep "v3" )
-# CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz"
-# # GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
-# # GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz"
+CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep $1 | awk -F '"' '{print $4}' | grep "v3" )
+CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz"
+# GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+# GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
 
-# wget -qO- $CLASH_DEV_URL | tar xOvz > $core_path/clash
-# wget -qO- $CLASH_TUN_URL | gunzip -c > $core_path/clash_tun
-# wget -qO- $CLASH_META_URL | tar xOvz > $core_path/clash_meta
-# # wget -qO- $GEOIP_URL > $goe_path/GeoIP.dat
-# # wget -qO- $GEOSITE_URL > $goe_path/GeoSite.dat
+wget -qO- $CLASH_DEV_URL | tar xOvz > $core_path/clash
+wget -qO- $CLASH_TUN_URL | gunzip -c > $core_path/clash_tun
+wget -qO- $CLASH_META_URL | tar xOvz > $core_path/clash_meta
+# wget -qO- $GEOIP_URL > $goe_path/GeoIP.dat
+# wget -qO- $GEOSITE_URL > $goe_path/GeoSite.dat
 
-# chmod +x $core_path/clash*
+chmod +x $core_path/clash*
 
 
 # git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git ./package/luci-app-unblockneteasemusic
